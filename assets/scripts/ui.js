d568412c4b38b52167df2e51ac485d51612b5edf
Maxwell Kwarteng <maxtheflash007@gmail.com>
2020-10-01 11:27:56 -0400

    Added the edited files to project2-client I added the edited ui.js file to project2-client.

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -2,20 +2,20 @@ const store = require('./store')
 const currentUser = require('./currentUser')
 
 const onSignUpSuccess = function (response) {
-  $('#message').text('Thanks for signing up ' + response.user.email).fadeIn(2000).fadeOut(2000)
+  $('#message').text('Thanks for signing up ' + response.user.email).fadeIn(6000).fadeOut(6000)
   $('#sign-up-form').trigger('reset')
   $('#my-login').show()
   $('#my-logout').hide()
 }
 
 const onSignUpFailure = function () {
-  $('#message').text('Sign up failed try again').fadeIn(2000).fadeOut(2000)
+  $('#message').text('Sign up failed try again').fadeIn(6000).fadeOut(6000)
   $('#my-login').show()
   $('#my-logout').hide()
 }
 const onSignInSuccess = function (response) {
   store.user = response.user
-  $('#message').text('Thanks for signing in ' + response.user.email + ' - Please scroll down to access your grocery list creator, no notepad needed! Remember to click Show after you Create, Edit or Delete. Happy Shopping!').fadeIn(2000).fadeOut(2000)
+  $('#message').text('Thanks for signing in ' + response.user.email + ' - Please scroll down to access your grocery list creator, no notepad needed! Remember to click Show after you Create, Edit or Delete. Happy Shopping!').fadeIn(6000).fadeOut(6000)
   $('#sign-in-form').trigger('reset')
   $('#change-password').show()
   $('#history').show()
@@ -29,13 +29,13 @@ const onSignInSuccess = function (response) {
   $('#my-logout').show()
 }
 const onSignInFailure = function () {
-  $('#message').text('Sign in failed try again').fadeIn(2000).fadeOut(2000)
+  $('#message').text('Sign in failed try again').fadeIn(6000).fadeOut(6000)
   $('#my-login').show()
   $('#my-logout').hide()
 }
 
 const onSignOutSuccess = function (response) {
-  $('#message').text('Thanks for signing out! ').fadeIn(2000).fadeOut(2000)
+  $('#message').text('Thanks for signing out! ').fadeIn(6000).fadeOut(6000)
   $('#sign-out-form').trigger('reset')
   $('#sign-up-form').hide()
   $('#sign-in-form').hide()
@@ -52,30 +52,30 @@ const onSignOutSuccess = function (response) {
 }
 
 const onSignOutFailure = function () {
-  $('#message').text('Sign out failed try again').fadeIn(2000).fadeOut(2000)
+  $('#message').text('Sign out failed try again').fadeIn(6000).fadeOut(6000)
   $('#my-login').hide()
   $('#my-logout').show()
 }
 
 const onChangePasswordSuccess = function () {
-  $('#message').text('Changed password successfully').fadeIn(2000).fadeOut(2000)
+  $('#message').text('Changed password successfully').fadeIn(6000).fadeOut(6000)
   $('#change-password').trigger('reset')
   $('#my-login').hide()
   $('#my-logout').show()
 }
 const onChangePasswordFailure = function () {
-  $('#message').text('Error on change password').fadeIn(2000).fadeOut(2000)
+  $('#message').text('Error on change password').fadeIn(6000).fadeOut(6000)
   $('#my-login').hide()
   $('#my-logout').show()
 }
 
 const onListCreateSuccess = function () {
-  $('#message-create').text('Grocery list created!').fadeIn(2000).fadeOut(2000)
+  $('#message-create').text('Grocery list created!').fadeIn(6000).fadeOut(6000)
   $('#grocery-list-form').trigger('reset')
 }
 
 const onListCreateFailure = function () {
-  $('#message-create').text('Grocery list was not created!').fadeIn(2000).fadeOut(2000)
+  $('#message-create').text('Grocery list was not created!').fadeIn(6000).fadeOut(6000)
   $('#grocery-list-form').trigger('reset')
 }
 
@@ -110,32 +110,32 @@ const onShowAllListSuccess = function (response) {
   })
   $('#listContent').empty()
   $('#listContent').html(groceriesHTML)
-  $('#message-show-all').text('Showing all lists!').fadeIn(2000).fadeOut(2000)
+  $('#message-show-all').text('Showing all lists!').fadeIn(6000).fadeOut(6000)
   $('form').trigger('reset')
 }
 
 const onShowAllListFailure = function () {
-  $('#message-show-all').text('Not displaying all lists!').fadeIn(2000).fadeOut(2000)
+  $('#message-show-all').text('Not displaying all lists!').fadeIn(6000).fadeOut(6000)
   $('form').trigger('reset')
 }
 
 const onListUpdateSuccess = function () {
-  $('#message-edit').text('Grocery list updated!').fadeIn(2000).fadeOut(2000)
+  $('#message-edit').text('Grocery list updated!').fadeIn(6000).fadeOut(6000)
   $('#edit').trigger('reset')
 }
 
 const onListUpdateFailure = function () {
-  $('#message-edit').text('Grocery list did not update!').fadeIn(2000).fadeOut(2000)
+  $('#message-edit').text('Grocery list did not update!').fadeIn(6000).fadeOut(6000)
   $('#edit').trigger('reset')
 }
 
 const onListDeleteSuccess = function () {
-  $('#message-delete').text('Grocery list deleted!').fadeIn(2000).fadeOut(2000)
+  $('#message-delete').text('Grocery list deleted!').fadeIn(6000).fadeOut(6000)
   $('#delete').trigger('reset')
 }
 
 const onListDeleteFailure = function () {
-  $('#message-delete').text('Grocery list did not delete!').fadeIn(2000).fadeOut(2000)
+  $('#message-delete').text('Grocery list did not delete!').fadeIn(6000).fadeOut(6000)
   $('#delete').trigger('reset')
 }
 
